42febbde0a9a1eeab2738792369a344261e5a890
Darien Tsai <smartdragondt@gmail.com>
2021-02-20 14:40:36 -0800

    fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+/* global google */
 import React from 'react';
 import { GoogleMap, LoadScript, HeatmapLayer } from '@react-google-maps/api';
 
@@ -6,6 +7,7 @@ export default function Home() {
 
   const [map, setMap] = React.useState(null)
   const [heat, setHeat] = React.useState(null)
+  const [points, setPoints] = React.useState([]);
 
   const onLoad = React.useCallback(function callback(map) {
     const bounds = new window.google.maps.LatLngBounds();
@@ -17,6 +19,39 @@ export default function Home() {
     const bounds = new window.google.maps.LatLngBounds();
     setHeat(map);
     console.log(map);
+    console.log(google);
+
+    setPoints([
+      new window.google.maps.LatLng(33.2987, -117.147, 5),
+      new window.google.maps.LatLng(33.3987, -117.247, 5),
+      new window.google.maps.LatLng(33.9987, -117.347, 5),
+      new window.google.maps.LatLng(33.5987, -117.447, 5),
+      new window.google.maps.LatLng(33.6987, -117.547, 5),
+      new window.google.maps.LatLng(33.7987, -117.647, 5),
+      new window.google.maps.LatLng(33.8987, -117.747, 5),
+      new window.google.maps.LatLng(33.9987, -117.847, 5),
+      new window.google.maps.LatLng(34.9987, -117.947, 5),
+      new window.google.maps.LatLng(34.2987, -116.947, 5),
+      new window.google.maps.LatLng(34.3987, -116.947, 5),
+      new window.google.maps.LatLng(34.4987, -116.947, 5),
+      new window.google.maps.LatLng(34.5987, -116.947, 5),
+      new window.google.maps.LatLng(34.6987, -116.947, 5),
+      new window.google.maps.LatLng(34.7987, -116.947, 5),
+      new window.google.maps.LatLng(34.8987, -116.947, 5),
+      new window.google.maps.LatLng(34.9987, -116.947, 5),
+      new window.google.maps.LatLng(34.9987, -118.947, 5),
+      new window.google.maps.LatLng(34.9987, -118.947, 5),
+      new window.google.maps.LatLng(32.9987, -118.947, 5),
+      new window.google.maps.LatLng(32.9987, -118.947, 5),
+      new window.google.maps.LatLng(32.9987, -118.947, 5),
+      new window.google.maps.LatLng(32.9987, -118.947, 5),
+      new window.google.maps.LatLng(32.9987, -118.947, 5),
+      new window.google.maps.LatLng(32.9987, -118.947, 5),
+      new window.google.maps.LatLng(32.9987, -118.947, 5)
+    ]);
+
+    // console.log(window.google);
+
   }, [])
 
   const onUnmount = React.useCallback(function callback(map) {
@@ -30,11 +65,11 @@ export default function Home() {
   };
 
   return (
-      <LoadScript googleMapsApiKey="KEYKEYKEYKEYKEYKEYKEYKEYKEYKEYKEYKEYKEYKEYKEYKEYKEYKEYKEYKEYKEYKEY" libraries={libs}>
+      <LoadScript googleMapsApiKey="AIzaSyAxVB1V2B-yeVM9OkUqJJM5Z2LrVc8gj0c" libraries={libs}>
         <GoogleMap
           mapContainerStyle={{"width": "100vw", "height": "100vh"}}
           center={center}
-          zoom={15}
+          zoom={17}
           onLoad={onLoad}
           onUnmount={onUnmount}
         >
@@ -45,22 +80,7 @@ export default function Home() {
             // optional
             onUnmount={onUnmount}
             // required
-            data={[
-              new window.google.maps.LatLng(37.782, -122.447),
-              new window.google.maps.LatLng(37.782, -122.445),
-              new window.google.maps.LatLng(37.782, -122.443),
-              new window.google.maps.LatLng(37.782, -122.441),
-              new window.google.maps.LatLng(37.782, -122.439),
-              new window.google.maps.LatLng(37.782, -122.437),
-              new window.google.maps.LatLng(37.782, -122.435),
-              new window.google.maps.LatLng(37.785, -122.447),
-              new window.google.maps.LatLng(37.785, -122.445),
-              new window.google.maps.LatLng(37.785, -122.443),
-              new window.google.maps.LatLng(37.785, -122.441),
-              new window.google.maps.LatLng(37.785, -122.439),
-              new window.google.maps.LatLng(37.785, -122.437),
-              new window.google.maps.LatLng(37.785, -122.435)
-            ]}
+            data={points}
           />
         </GoogleMap>
       </LoadScript>
