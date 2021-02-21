4e2192977f9cfcdf8260fa0988622ff3491c5a8b
unknown <jeffreyliu02@gmail.com>
2021-02-20 23:29:03 -0800

    added markers, density calculations

--- src/pages/Heatmap.js
+++ src/pages/Heatmap.js
@@ -1,5 +1,5 @@
 /*
-    Authors: Jack Chou, Jeffrey Liu, Darien Tsai
+Authors: Jack Chou, Jeffrey Liu, Darien Tsai
 */
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
@@ -7,6 +7,7 @@ import firebase from "firebase/app";
 import 'firebase/database'
 import io from 'socket.io-client';
 import {APIKEY, FBKEY} from '../key'
+import MapPin from '../components/MapPin'
 
 console.log(firebase);
 let app = firebase.initializeApp({
@@ -18,8 +19,8 @@ let app = firebase.initializeApp({
 class HeatMap extends Component {
     static defaultProps = {
         center: {
-            lat: 33.998470127751006,
-            lng: -117.94746979872437
+            lat: 33.986217,
+            lng: -117.976234
         },
         zoom: 15
     };
@@ -36,7 +37,33 @@ class HeatMap extends Component {
                     opacity: 0.3
                 }
             },
+            lat: 33.986217,
+            lng: -117.976234,
+            dense:""
         };
+    }
+    denseCalc = (data) => {
+        let nearby = 0;
+        let denseScale = ["spacious", "moderate", "dense"];
+        for(let i = 0; i < data.length; i++) {
+            let x = Math.abs(data[i][0]-this.state.lat);
+            let y = Math.abs(data[i][1]-this.state.lng);
+            if(Math.sqrt(x*x+y*y) < 0.0015){
+                nearby += 1;
+            }
+        }
+        let fiDense = (Math.floor(nearby/12) > 2)?2:Math.floor(nearby/12);
+        console.log(nearby +": " +denseScale[fiDense]);
+        this.setState({dense: denseScale[fiDense]});
+    }
+    showPosition = (position) => {
+        console.log(position.coords.latitude);
+        console.log(position.coords.longitude);
+        this.setState({lat: position.coords.latitude});
+        this.setState({lng: position.coords.longitude});
+        let fiData = this.state.heatMapData;
+        fiData["positions"].push({lat: position.coords.latitude, lng: position.coords.longitude});
+        this.setState({heatMapData: fiData, mapStatus: true});
     }
 
     addPoints = (data) => {
@@ -44,7 +71,9 @@ class HeatMap extends Component {
         for(let i = 0; i < data.length; i++) {
             newData["positions"].push({lat: data[i][0], lng: data[i][1]});
         }
-        this.setState({heatMapData: newData, mapStatus: true});
+        navigator.geolocation.getCurrentPosition(this.showPosition, console.error,  {maximumAge:10000, timeout:5000, enableHighAccuracy: true});
+        this.setState({heatMapData: newData});
+        this.denseCalc(data);
     }
     render() {
         return (
@@ -57,22 +86,27 @@ class HeatMap extends Component {
                 heatmapLibrary={true}
                 heatmap={this.state.heatMapData}
                 >
-            </GoogleMapReact>) : (<></>)
-        );
+                <MapPin
+                style={{width:5}}
+                lat={this.state.lat}
+                lng={this.state.lng}
+                ></MapPin>
+                </GoogleMapReact>) : (<></>)
+            );
+        }
+        componentDidMount() {
+            const socket = io('https://crowdmap-server.herokuapp.com');
+
+            socket.on('connect', () =>{
+                socket.emit("update", {"coord": [100, 100]});
+            });
+            socket.on('get', (data) =>{
+                this.addPoints(data);
+                // for(let i = 0; i < data.length; i ++)
+                  // firebase.database().ref("record/" + i).set(data[i]).catch(console.error).then(console.log("pushed"));
+            });
+
+        }
     }
-    componentDidMount() {
-        const socket = io('https://crowdmap-server.herokuapp.com');
 
-        socket.on('connect', () =>{
-          socket.emit("update", {"coord": [100, 100]});
-        });
-        socket.on('get', (data) =>{
-          this.addPoints(data);
-          // for(let i = 0; i < data.length; i ++)
-          //   firebase.database().ref("record/" + i).set(data[i]).catch(console.error);
-        });
-
-    }
-}
-
-export default HeatMap;
+    export default HeatMap;
