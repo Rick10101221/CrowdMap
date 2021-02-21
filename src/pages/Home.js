2ffb6338f30a8d696b3c5929c56d3cd843e96483
Rick10101221 <rikhilna@ucsd.edu>
2021-02-20 16:32:22 -0800

    added home animations and springs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled, { keyframes } from 'styled-components';
-import { bounce } from 'react-animations';
 import { useHistory } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import { withStyles, makeStyles } from '@material-ui/core/styles';
 import { blue } from '@material-ui/core/colors';
+import { useTrail, a } from 'react-spring'
 
-
-import { LinkedButton } from '../components/LinkedButton';
 import crowdLogo from '../images/crowdLogo.png';
 import mapLogo from '../images/mapLogo.png';
 
@@ -16,10 +14,7 @@ import '../styles/general_text.css';
 
 export const Home = () => {
   const history = useHistory();
-
-  const handleClickOpen = () => {
-    history.push('/map')
-  };
+  const [open, setOpen] = useState(true);
 
   const useStyles = makeStyles((theme) => ({
     margin: {
@@ -28,6 +23,24 @@ export const Home = () => {
   }));
 
   const classes = useStyles();
+
+  let crowdImg = (
+    <img 
+      src={crowdLogo} 
+      alt='Crowd Logo'
+      width='300'
+      height='112'
+    />
+  )
+  let mapImg = (
+    <img
+      src={mapLogo}
+      alt='Map Logo'
+      width='225'
+      height='121'
+    />
+  )
+  const items = [crowdImg, mapImg];
 
   const ColorButton = withStyles((theme) => ({
     root: {
@@ -39,22 +52,44 @@ export const Home = () => {
     },
   }))(Button);
 
+  // For trail speed, alter tension
+  const trail = useTrail(items.length, {
+    config: { mass: 5, tension: 800, friction: 200 },
+    opacity: open ? 1 : 0,
+    x: open ? 0 : 20,
+    height: open ? 110 : 0,
+    from: { opacity: 0, x: 20, height: 0 },
+  })
+
+  const handleClickOpen = () => {
+    history.push('/map')
+  };
+
   return (
     <div id='top-level-home'>
+
       <div id='images'>
-        <img 
-          src={crowdLogo} 
-          alt='Crowd Logo'
-          width='300'
-          height='112'
-        />
-        <img
-          src={mapLogo}
-          alt='Map Logo'
-          width='225'
-          height='121'
-        />
+        <div 
+          className="trails-main" 
+          onClick={() => setOpen((state) => !state)}
+        >
+          <div>
+            {trail.map(({ x, height, ...rest }, index) => (
+              <a.div
+                key={items[index]}
+                className="trails-text"
+                id='center'
+                style={{ 
+                  ...rest, 
+                  transform: x.interpolate((x) => `translate3d(0,${x}px,0)`) 
+                }}>
+                <a.div style={{ height }}>{items[index]}</a.div>
+              </a.div>
+            ))}
+          </div>
+        </div>
       </div>
+
       <div id='button'>
         <ColorButton 
           variant="contained" 
@@ -65,12 +100,7 @@ export const Home = () => {
           BUTTON
         </ColorButton>
       </div>
+      
     </div>
   )
 }
-
-const bounceAnimation = keyframes`${bounce}`;
-
-const BouncySpan = styled.span`
-  animation: 1s ${bounceAnimation};
-`;
--- src/pages/MainMap.js
+++ src/pages/MainMap.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { FormDialog } from '../components/FormDialog';
 import { Header } from '../components/Header';
@@ -6,15 +6,32 @@ import { GetGPSModal } from '../components/GetGPSModal';
 import { render } from 'react-dom'
 import { useTrail, a } from 'react-spring'
 
+import crowdLogo from '../images/crowdLogo.png';
+import mapLogo from '../images/mapLogo.png';
 import '../styles/mainmap.css';
 
 export const MainMap = () => {
+
   return (
     <div>
       <Header 
         density='dense'
       />
       <GetGPSModal />
+
+      {/* <div className="trails-main" onClick={() => setOpen((state) => !state)}>
+        <div>
+          {trail.map(({ x, height, ...rest }, index) => (
+            <a.div
+              key={items[index]}
+              className="trails-text"
+              style={{ ...rest, transform: x.interpolate((x) => `translate3d(0,${x}px,0)`) }}>
+              <a.div style={{ height }}>{items[index]}</a.div>
+            </a.div>
+          ))}
+        </div>
+      </div> */}
+
       <div id='location-button'>
         <FormDialog />
       </div>
