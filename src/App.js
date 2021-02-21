02fb73fa4041123096c9f04660903187164f2d66
unknown <jeffreyliu02@gmail.com>
2021-02-20 16:33:40 -0800

    made heatmap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
-import { Home } from './pages/Home';
+import SimpleMap from './pages/SimpleMap';
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <Switch>
         <Route exact path='/' render={()=> <Redirect to="/home"/>} />
-        <Route exact path='/home' component={Home} />
+        <Route exact path='/home' component={SimpleMap}/>
       </Switch>
     </Router>
     </div>
